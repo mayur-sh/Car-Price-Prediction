96f4bdaafc43e7a51925aaa09332a9dbb2fd7658
Mayur Shrotriya <103096253+mayur-sh@users.noreply.github.com>
2023-12-18 18:42:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(page_title='The No Code ML App', page_icon='atom.png', layout
 
 embed_component = {
     'Linkedin': """<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-    <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="mayur-shrotriya" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/mayur-shrotriya?trk=profile-badge"></a></div>""" 
+    <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="mayur-sh" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/mayur-sh?trk=profile-badge"></a></div>""" 
 }
 yellow_background = "<style>:root {background-color: #262730;}</style>"
 
